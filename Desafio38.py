f1c03967f19936005e4d882b4444bcd7ad082b1b
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 16:30:41 -0300

    Add files via upload

---
+++ Desafio38.py
@@ -0,0 +1,9 @@
+# Compara dois números digitados e retorna com o maior e menor.
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+if n1 > n2:
+    print('O PRIMEIRO número é MAIOR.')
+elif n2 > n1:
+    print('O SEGUNDO número é MAIOR.')
+else:
+    print('Os DOIS números são iguais.')
