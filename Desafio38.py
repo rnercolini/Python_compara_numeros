5527b8c822dad2d954f49cb3c98c6d806b9081ac
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 16:29:24 -0300

    Add files via upload

---
+++ Desafio38.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+if n1 > n2:
+    print('O PRIMEIRO número é MAIOR.')
+elif n2 > n1:
+    print('O SEGUNDO número é MAIOR.')
+else:
+    print('Os DOIS números são iguais.')
